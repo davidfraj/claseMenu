22fc6a3b4658c42e35685555b0818c30b43dca8f
davidfraj <davidfraj@gmail.com>
2017-01-11 13:14:00 +0100

    Add files via upload

--- menu.class.php
+++ menu.class.php
@@ -1,8 +1,6 @@
-﻿<?php
+<?php
 /*
-//Version del archivo a fecha 2017 - Enero - 11
-
-
+Lo que sea
 menu.class.php, adaptado a BOOTSTRAP
 Cómo usaré esta clase:
  $p será una variable, que contendrá la página que esta mostrando el index en este momento, de manera que se recogera en el index asi:
